aa595683e8a714868f39acbec65babfcaeb60670
jcrozum <jordan.rozum@pnnl.gov>
2024-09-10 14:13:17 -0700

    add test to see if structure calculations complete

--- tests/test_protein_class.py
+++ tests/test_protein_class.py
@@ -80,6 +80,21 @@ def test_fetch_pdb():
     protein.fetch_pdb(save_path="tests/test_data/outputs/test_pdb.pdb")
 
 
+def test_structure_run_only():
+    TEST_HEADER = "Entry	Reviewed	Entry Name	Protein names	Gene Names	Organism	Length	Sequence	Active site	Binding site	DNA binding	Disulfide bond	Beta strand	Helix	Turn"
+    TEST_ROW = "A0A0B4J2F0	reviewed	PIOS1_HUMAN	Protein PIGBOS1 (PIGB opposite strand protein 1)	PIGBOS1	Homo sapiens (Human)	54	MFRRLTFAQLLFATVLGIAGGVYIFQPVFEQYAKDQKELKEKMQLVQESEEKKS							"
+
+    row_dict = {k: v for k, v in zip(TEST_HEADER.split("\t"), TEST_ROW.split("\t"))}
+    protein = Protein.from_uniprot_row(row_dict)
+    protein.fetch_pdb(save_path="tests/test_data/outputs/test_pdb.pdb")
+
+    protein.get_charge()
+    protein.get_sasa()
+    protein.get_size()
+    # protein.get_titration() # optional dependency
+    protein.get_titration_estimate()
+
+
 def test_uniprot_api():
     df = pd.read_csv(  # type: ignore
         TEST_DATA_PATH,
