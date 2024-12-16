3f1d38b239273ffa96b111bd04020cc3cc1224e3
Jordan Rozum <jordan.rozum@pnnl.gov>
2024-12-16 14:23:49 -0800

    updated docstrings

--- procaliper/view/nglview_utils.py
+++ procaliper/view/nglview_utils.py
@@ -13,6 +13,20 @@ except ImportError:
 
 
 def protein_to_nglview(protein: Protein) -> nglview.NGLWidget:
+    """Generates an nglview widget from a protein that has an associated PDB file.
+
+    Must run `protein.fetch_pdb` first or specify an abosulute path to the PDB
+    in `protein.pdb_location_absolute`.
+
+    Args:
+        protein (Protein): The protein object to visualize.
+
+    Raises:
+        ValueError: If the PDB location is not set.
+
+    Returns:
+        nglview.NGLWidget: an nglview widget
+    """
     if not protein.pdb_location_absolute:
         raise ValueError("PDB location not set; use `fetch_pdb` first")
     return nglview.show_file(protein.pdb_location_absolute)
@@ -35,6 +49,22 @@ def ngl_scheme(
     float_to_hex: Callable[[float], str] | None = None,
     two_sided: bool = False,
 ) -> list[tuple[str, str]]:
+    """Converts a list of values to an nglview color scheme.
+
+    Args:
+        data (list[float]): The list of values to convert.
+        float_to_hex (Callable[[float], str] | None, optional): Function that
+            converts a float to a hex color in the form `"#RRGGBB"`. If `None`,
+            a default function is used that interpolates between white and green
+            (one-sided) or red and blue (two-sided). Defaults to `None`.
+        two_sided (bool, optional): Whether to use a two-sided color scheme. If
+            `False`, we assume `data` only contains positive values. Defaults to
+            `False`.
+
+    Returns:
+        list[tuple[str, str]]: A list of color and residue number tuples that
+            are compatible with nglview.
+    """
     if float_to_hex is None:
         if two_sided:
             float_to_hex = _default_float_to_hex_rb
