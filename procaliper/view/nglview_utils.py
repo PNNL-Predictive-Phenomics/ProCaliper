5efdd10870bda8b3902577e470ed5683456be80a
Jordan Rozum <jordan.rozum@pnnl.gov>
2025-09-30 10:48:04 -0700

    updated demo and made color mapping more user-friendly

--- procaliper/view/nglview_utils.py
+++ procaliper/view/nglview_utils.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import Callable
 
+import matplotlib as plt
+
 from .._protein import Protein
 
 """
@@ -49,39 +51,58 @@ def _default_float_to_hex_rb(x: float) -> str:
         return f"#{int(1*255):02x}{int((1-x)*255):02x}{int((1-x)*255):02x}"
 
 
+def _matplotlib_cmapper(cmap: str, two_sided: bool) -> Callable[[float], str]:
+    if two_sided:
+        return lambda x: plt.colors.to_hex(plt.colormaps.get_cmap(cmap)(x / 2 + 0.5))
+
+    return lambda x: plt.colors.to_hex(plt.colormaps.get_cmap("viridis_r")(x))
+
+
 def ngl_scheme(
     data: list[float],
-    float_to_hex: Callable[[float], str] | None = None,
+    color_mapper: Callable[[float], str] | None = None,
     two_sided: bool = False,
+    rescale: bool = True,
 ) -> list[tuple[str, str]]:
     """Converts a list of values to an nglview color scheme.
 
     Args:
         data (list[float]): The list of values to convert.
-        float_to_hex (Callable[[float], str] | None, optional): Function that
-            converts a float to a hex color in the form `"#RRGGBB"`. If `None`,
-            a default function is used that interpolates between white and green
-            (one-sided) or red and blue (two-sided). Defaults to `None`.
+        color_mapper (Callable[[float], str] | str | None, optional): Function that
+            converts a float to a hex color in the form `"#RRGGBB"`. If a string, it
+            should be the name of a matplotlib colormap. If `None`, a default function
+            is used that interpolates between white and green (one-sided) or red and blue (two-sided).
+            Defaults to `None`.
         two_sided (bool, optional): Whether to use a two-sided color scheme. If
             `False`, we assume `data` only contains positive values. Defaults to
             `False`.
+        rescale (bool, optional): Whether to rescale the values to be between
+            0 and 1 (one-sided) or -1 and 1 (two-sided). Defaults to `True`.
+            If `False`, the values are not rescaled, but are assumed to fall within
+            [0, 1] or [-1, 1] depending on `two_sided`.
 
     Returns:
         list[tuple[str, str]]: A list of color and residue number tuples that
             are compatible with nglview.
     """
-    if float_to_hex is None:
+    if color_mapper is None:
         if two_sided:
-            float_to_hex = _default_float_to_hex_rb
+            color_mapper = _default_float_to_hex_rb
         else:
-            float_to_hex = _default_float_to_hex
+            color_mapper = _default_float_to_hex
 
-    maxx = max(data)
-    scale = max(min(data), abs(maxx)) if two_sided else maxx
+    if isinstance(color_mapper, str):
+        color_mapper = _matplotlib_cmapper(color_mapper, two_sided)
 
-    if scale == 0:
-        data_scaled = [0.0] * len(data)
+    if rescale:
+        maxx = max(data)
+        scale = max(min(data), abs(maxx)) if two_sided else maxx
+
+        if scale == 0:
+            data_scaled = [0.0] * len(data)
+        else:
+            data_scaled = [x / maxx for x in data]
     else:
-        data_scaled = [x / maxx for x in data]
+        data_scaled = data
 
-    return [(float_to_hex(x), f"{i+1}") for i, x in enumerate(data_scaled)]
+    return [(color_mapper(x), f"{i+1}") for i, x in enumerate(data_scaled)]
